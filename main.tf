f4088706bad5919d04ef2d33c80df799dc3e0d21
tqer39 <tqer39@gmail.com>
2021-08-11 15:35:29 +0900

    version.tfの内容をmain.tfに移した。version.tfは削除

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
