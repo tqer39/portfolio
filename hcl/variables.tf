603647a2b4bb8d4049e0090043d8112d511d3baf
tqer39 <tqer39@gmail.com>
2021-08-12 17:42:38 +0900

    AZの変数を追加

--- hcl/variables.tf
+++ hcl/variables.tf
@@ -44,3 +44,12 @@ variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
   default     = "10.0.0.0/16"
 }
+
+variable "availability_zones" {
+  description = "Availability Zones"
+  default = [
+    "ap-northeast-1a",
+    "ap-northeast-1c",
+    "ap-northeast-1d",
+  ]
+}
