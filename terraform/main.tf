d4c9097fa6dd2d02946ac2b79d5d79297f9b2694
tqer39 <tqer39@gmail.com>
2021-08-26 06:30:32 +0900

    Route53のレコードを作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,22 +173,22 @@ resource "aws_route53_zone" "portfolio" {
   force_destroy = true
 }
 
-# resource "aws_route53_record" "portfolio_cname" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.portfolio.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+resource "aws_route53_record" "portfolio" {
+  for_each = {
+    for dvo in aws_acm_certificate.portfolio.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = aws_route53_zone.portfolio.id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.portfolio.id
+}
 
 # resource "aws_s3_bucket" "cloudfront_log" {
 #   bucket        = "cloudfront-log-373303485727"
