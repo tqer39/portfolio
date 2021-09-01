10032b32bb378f1d11b5c6402fcae9a284e3bbd6
tqer39 <tqer39@gmail.com>
2021-09-02 05:41:14 +0900

    Headingコンポーネントにサイト名を設定

--- typescript/src/pages/components/Navbar.tsx
+++ typescript/src/pages/components/Navbar.tsx
@@ -1,11 +1,13 @@
-// import React from "react";
 import type { NextPage } from "next";
-import { Avatar } from "@chakra-ui/react";
+import { Avatar, Heading } from "@chakra-ui/react";
 import gravatarUrl from "gravatar-url";
 
 const Header: NextPage = (props) => {
   return (
     <>
+      <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
+        Takeru O'oyama
+      </Heading>
       <Avatar
         size={"md"}
         src={gravatarUrl("tqer39@gmail.com", { size: 100 })}
