6a4cf6942a2e14bb515858d2f7fbb3b6b6f35622
tqer39 <tqer39@gmail.com>
2021-09-02 05:48:22 +0900

    dark/lightのテーマ切り替えの機能を追加

--- typescript/src/pages/components/Navbar.tsx
+++ typescript/src/pages/components/Navbar.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 import { Avatar, Heading } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { IconButton } from "@chakra-ui/button";
+import { FaSun, FaMoon } from "react-icons/fa";
 import gravatarUrl from "gravatar-url";
 
 const Header: NextPage = (props) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return (
     <>
       <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
@@ -12,6 +18,13 @@ const Header: NextPage = (props) => {
         size={"md"}
         src={gravatarUrl("tqer39@gmail.com", { size: 100 })}
       />
+      <IconButton
+        ml={8}
+        aria-label=""
+        icon={isDark ? <FaSun /> : <FaMoon />}
+        isRound={true}
+        onClick={toggleColorMode}
+      ></IconButton>
     </>
   );
 };
--- typescript/src/pages/index.tsx
+++ typescript/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import Layout from "./components/Layout";
 
 const Home: NextPage = () => {
   return (
     <ChakraProvider>
+      <ColorModeScript initialColorMode="light"></ColorModeScript>
       <Layout />
     </ChakraProvider>
   );
