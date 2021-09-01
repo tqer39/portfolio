51c4c5487d4a472a6b8f51c0f95f8e3313223aad
tqer39 <tqer39@gmail.com>
2021-09-02 05:17:17 +0900

    画面サイズ横幅一杯を使用するためFlexコンポーネントのwで100%を指定

--- typescript/src/pages/components/Layout.tsx
+++ typescript/src/pages/components/Layout.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from "next";
+import { Flex } from "@chakra-ui/react";
 import { VStack } from "@chakra-ui/layout";
 import Navbar from "./Navbar";
 
 const Layout: NextPage = () => {
   return (
     <VStack>
-      <Navbar />
-      <h1>About</h1>
-      Profile Image.
-      <hr />
-      <h1>Skill</h1>
-      <hr />
-      <h1>Profile</h1>
-      <footer>copyright</footer>
+      <Flex w="100%">
+        <Navbar />
+        <h1>About</h1>
+        Profile Image.
+        <hr />
+        <h1>Skill</h1>
+        <hr />
+        <h1>Profile</h1>
+        <footer>copyright</footer>
+      </Flex>
     </VStack>
   );
 };
